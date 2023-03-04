a23a20a485c746b3dc82d7acbb7a9aa31ea52bb8
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-03-04 17:42:14 -0500

    (very) minor refactor

--- main.ts
+++ main.ts
@@ -370,11 +370,11 @@ export default class LatexAlgorithms extends Plugin {
 				if (!this.withinMath(editor)) return false;
 
 				const currLine = editor.getLine(editor.getCursor().line);
-				let commandCutoff = 0;
+				let cutOff = 0;
 
-				while (currLine[commandCutoff] === "&") commandCutoff++;
+				while (currLine[cutOff] === "&") cutOff++;
 
-				editor.setSelection({line:editor.getCursor().line, ch:commandCutoff});
+				editor.setSelection({line:editor.getCursor().line, ch:cutOff});
 				editor.replaceSelection("\\quad ");
 				editor.setCursor({line:editor.getCursor().line, ch:editor.getLine(editor.getCursor().line).length-1}); 
 
